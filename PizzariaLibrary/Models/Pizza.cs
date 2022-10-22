1fd348f6c7212f2f459fac2a0f916a737ca404a8
Arthuthu <arthurgeromello@hotmail.com>
2022-10-22 01:06:54 -0300

    Data anotations na pizza model

--- PizzariaLibrary/Models/Pizza.cs
+++ PizzariaLibrary/Models/Pizza.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,17 @@ namespace PizzariaLibrary.Models
     public class Pizza
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "É necessário preencher o campo do nome")]
         public string Nome { get; set; }
+        [DisplayName("Descrição")]
+        [Required(ErrorMessage = "É necessário preencher o campo da descrição")]
+
         public string Descricao { get; set; }
+        [Required(ErrorMessage = "É necessário preencher o campo do tipo da pizza")]
+
         public string Tipo { get; set; }
+        [Required(ErrorMessage = "É necessário preencher o campo do valor")]
+
         public decimal Valor { get; set; }
 
     }
