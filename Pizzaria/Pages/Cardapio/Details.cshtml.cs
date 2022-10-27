b3d456a2b5a6448e9a80e087dd067fd058ac252a
Arthuthu <arthurgeromello@hotmail.com>
2022-10-27 04:52:58 -0300

    Details funcionando corretamente, terminar Delete e Update later

--- Pizzaria/Pages/Cardapio/Details.cshtml.cs
+++ Pizzaria/Pages/Cardapio/Details.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzariaLibrary.Models;
 using PizzariaLibrary.Repositories;
 
 namespace Pizzaria.Pages.Cardapio
@@ -9,6 +10,8 @@ namespace Pizzaria.Pages.Cardapio
         private readonly ILogger<CardapioModel> _logger;
         private readonly IPizzaRepository _pizzaRepository;
 
+        public Pizza PizzaItem{ get; set; }
+
         public DetailsModel(ILogger<CardapioModel> logger, IPizzaRepository pizzaRepository)
         {
             _logger = logger;
@@ -16,6 +19,7 @@ namespace Pizzaria.Pages.Cardapio
         }
         public void OnGet(int id)
         {
+            PizzaItem = _pizzaRepository.Search(id);
             _logger.LogInformation("Loading the details page");
         }
     }
