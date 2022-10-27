b52f273c0d597fb936130b59d1a10e3323188265
Arthuthu <arthurgeromello@hotmail.com>
2022-10-27 04:36:33 -0300

    Teste

--- Pizzaria/Pages/Cardapio/Details.cshtml.cs
+++ Pizzaria/Pages/Cardapio/Details.cshtml.cs
@@ -1,12 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzariaLibrary.Repositories;
 
 namespace Pizzaria.Pages.Cardapio
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly ILogger<CardapioModel> _logger;
+        private readonly IPizzaRepository _pizzaRepository;
+
+        public DetailsModel(ILogger<CardapioModel> logger, IPizzaRepository pizzaRepository)
         {
+            _logger = logger;
+            _pizzaRepository = pizzaRepository;
+        }
+        public void OnGet(int id)
+        {
+            _logger.LogInformation("Loading the details page");
         }
     }
 }
