345290a04ee46f9cc7da0190873600cef936d345
Arthuthu <arthurgeromello@hotmail.com>
2022-10-28 17:53:39 -0300

    Added some unit tests

---
+++ PizzariaSiteTests/CardapioTests.cs
@@ -0,0 +1,43 @@
+﻿using Castle.Core.Logging;
+using FluentAssertions;
+using NSubstitute;
+using PizzariaLibrary.Models;
+using PizzariaLibrary.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzariaSiteTests
+{
+    public class CardapioTests
+    {
+        private readonly IPizzaRepository _pizzaRepository;
+
+        public CardapioTests(IPizzaRepository pizzaRepository)
+        {
+            _pizzaRepository = pizzaRepository;
+        }
+
+        [Fact]
+        public void CardapioCreate_ShouldCreatePizza_WhenAllParametersAreValid()
+        {
+            //Arrange
+            var pizzaModel = new Pizza
+            {
+                Id = 1,
+                Nome = "Calabresa",
+                Descricao = "Ingredientes",
+                Tipo = "Salgada",
+                Valor = 23
+            };
+
+            //Act
+            var result = _pizzaRepository.Create(pizzaModel);
+
+            //Assert
+            //result.Should().BeTrue();
+        }
+    }
+}
