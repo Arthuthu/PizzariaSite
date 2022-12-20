3605863155e7bd3d56c41d4d0fd094bdaf087cdd
Arthuthu <arthurgeromello@hotmail.com>
2022-12-20 16:02:13 -0300

    Created SqlDataAccess Class and Interface

---
+++ PizzariaLibrary/DbAccess/ISqlDataAccess.cs
@@ -0,0 +1,8 @@
+﻿namespace PizzariaLibrary.DbAccess
+{
+    public interface ISqlDataAccess
+    {
+        Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionId = "Pizzaria");
+        Task SaveData<T>(string storedProcedure, T parameters, string connectionId = "Pizzaria");
+    }
+}
