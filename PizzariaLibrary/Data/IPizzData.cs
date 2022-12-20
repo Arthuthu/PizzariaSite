658713cc75bd1bd1aa0b2e62755d5d96449f4114
Arthuthu <arthurgeromello@hotmail.com>
2022-12-20 17:04:35 -0300

    Added PizzaData class and Interface  (does the same thing as PizzaRepository but with differente code)

---
+++ PizzariaLibrary/Data/IPizzData.cs
@@ -0,0 +1,13 @@
+﻿using PizzariaLibrary.Models;
+
+namespace PizzariaLibrary.Data
+{
+	public interface IPizzData
+	{
+		Task DeletePizza(int id);
+		Task<IEnumerable<PizzaModel>> GetAllPizzas();
+		Task<PizzaModel?> GetPizzaById(int id);
+		Task InsertPizza(PizzaModel pizza);
+		Task UpdatePizza(PizzaModel pizza);
+	}
+}
---
+++ PizzariaLibrary/Data/PizzData.cs
@@ -0,0 +1,48 @@
+﻿using PizzariaLibrary.DbAccess;
+using PizzariaLibrary.Models;
+
+namespace PizzariaLibrary.Data
+{
+	public class PizzData : IPizzData
+	{
+		private readonly ISqlDataAccess _db;
+
+		public PizzData(ISqlDataAccess db)
+		{
+			_db = db;
+		}
+
+		public Task<IEnumerable<PizzaModel>> GetAllPizzas()
+		{
+			return _db.LoadData<PizzaModel, dynamic>("dbo.spPizza_GetAll", new { });
+		}
+
+		public async Task<PizzaModel?> GetPizzaById(int id)
+		{
+			var results = await _db.LoadData<PizzaModel, dynamic>("dbo.spPizza_Get", new { Id = id });
+
+			return results.FirstOrDefault();
+		}
+
+		public Task InsertPizza(PizzaModel pizza)
+		{
+			return _db.SaveData("dbo.spPizza_Create", new
+			{
+				pizza.Nome,
+				pizza.Descricao,
+				pizza.Tipo,
+				pizza.Valor
+			});
+		}
+
+		public Task UpdatePizza(PizzaModel pizza)
+		{
+			return _db.SaveData("dbo.spPizza_Update", pizza);
+		}
+
+		public Task DeletePizza(int id)
+		{
+			return _db.SaveData("dbo.spPizza_Delete", new { Id = id });
+		}
+	}
+}
